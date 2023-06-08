c3456d47a6d49c1214fa59c0c8d977e4d13c75ef
Mayank Sharma <mayankshrm101@gmail.com>
2023-06-08 18:03:06 +0530

    app folder add

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import express from "express";
-import pgRoute from "../server/routes/pg.js";
+import pgRoute from "../routes/pg.js";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
